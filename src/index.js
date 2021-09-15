7a0f746dcd433fa2d4a5334f1ad7f0df32b64fbf
1zemzem <1zemliakova@gmail.com>
2021-09-15 14:03:19 +0300

    base

--- src/index.js
+++ src/index.js
@@ -1,17 +1,11 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+const getResourse = async (url) => {
+  const res = await fetch(url);
+  const body = await res.json();
+  return body;
+};
 
-ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
-);
+getResourse('https://swapi.dev/api/people/1')
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+  .then((body) => {
+    console.log(body)
+  });
